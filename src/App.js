adb986394376a5350709de213f2756779290d786
anees50hertz <anees50hertz@gmail.com>
2023-08-14 10:53:57 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import setting from './assets/icons/setting.svg'
+import reports from './assets/icons/reports.svg'
+import Button from '@mui/material/Button';
+import Navbar from './components/navbar/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      {/* <img src={setting} alt='logo'/>
+      <img src={reports} alt='logo'/>
+      <div>
+        <Button variant="contained">Hello World</Button>
+      </div> */}
     </div>
   );
 }
