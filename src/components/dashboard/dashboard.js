6009ab3031ea3206a56b3d54066d126102ba5f00
anees50hertz <anees50hertz@gmail.com>
2023-08-20 04:12:20 +0200

    Fix the buttons,  text, images and margins of the cards  CSS.

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -20,35 +20,36 @@ const Dashboard = (props) => {
             logo: bolt,
             name: 'Substation name',
             status: 'Pending',
-            date: 'Change detected - 08/06/22',
+            date: 'Change detected - 08/06/23',
             description: 'Go to substation to review change',
             zone: 'Zone S',
-            region: 'Region XYZ'
+            region: 'Region XYZ',
+            balance: 'Other balances affected'
         }, {
             logo: frame,
             name: 'Balance name',
-            status: 'Pending',
-            date: 'Error detected - 08/06/22',
+            date: 'Error detected - 08/06/23',
             description: 'Go to substation to review change',
             zone: 'Zone S',
-            region: 'Region XYZ'
+            region: 'Region XYZ',
+            balance: 'Other balances affected'
         },
         {
             logo: frame,
             name: 'NA-VI4',
-            status: 'Cancel',
-            date: 'Error detected - 08/06/22',
+            date: 'Error detected - 08/06/23',
             description: 'Go to substation to review change',
             zone: 'Zone S',
-            region: 'Region XYZ'
+            region: 'Region XYZ',
+            balance: 'Other balances affected'
         }, {
             logo: bolt,
             name: 'NA-VI5',
-            status: 'Pending',
-            date: 'Change detected - 08/06/22',
+            date: 'Change detected - 08/06/23',
             description: 'Go to substation to review change',
             zone: 'Zone S',
-            region: 'Region XYZ'
+            region: 'Region XYZ',
+            balance: 'Other balances affected'
         },
     ]
 
@@ -58,6 +59,10 @@ const Dashboard = (props) => {
             name: 'SUBSTATION 70',
             button: 'Go to substation',
             zone: 'Zone N',
+            zonestyle:{
+                color: 'var(--colors-success-fg-on-subtle, #138719)',
+                background: 'var(--colors-success-bg-subtle, #E2F0E2)'
+            },
             region: 'Region XYZ',
             regionFlag: flag,
         },
@@ -66,51 +71,84 @@ const Dashboard = (props) => {
             name: 'Region A',
             button: 'Go to region',
             zone: 'Zone S',
+            zonestyle:{
+                color: 'var(--colors-primary-fg-on-subtle, #E75420)',
+                background: 'var(--colors-primary-bg-subtle, #FCEAE3)'
+            },
         },
         {
             logo:flag,
             name: 'Region B',
             button: 'Go to region',
             zone: 'Zone S',
+            zonestyle:{
+                color: 'var(--colors-primary-fg-on-subtle, #E75420)',
+                background: 'var(--colors-primary-bg-subtle, #FCEAE3)'
+            },
         },
         {
             logo:flag,
             name: 'Region C',
             button: 'Go to region',
             zone: 'Zone S',
+            zonestyle:{
+                color: 'var(--colors-primary-fg-on-subtle, #E75420)',
+                background: 'var(--colors-primary-bg-subtle, #FCEAE3)'
+            },
         },
         {
             logo:flag,
             name: 'Region D',
             button: 'Go to region',
             zone: 'Zone S',
+            zonestyle:{
+                color: 'var(--colors-primary-fg-on-subtle, #E75420)',
+                background: 'var(--colors-primary-bg-subtle, #FCEAE3)'
+            },
         },
         {
             logo:flag,
             name: 'Region W',
             button: 'Go to region',
             zone: 'Zone S',
+            zonestyle:{
+                color: 'var(--colors-primary-fg-on-subtle, #E75420)',
+                background: 'var(--colors-primary-bg-subtle, #FCEAE3)'
+            },
         },
         {
             logo:flag,
             name: 'Region X',
             button: 'Go to region',
             zone: 'Zone S',
+            zonestyle:{
+                color: 'var(--colors-primary-fg-on-subtle, #E75420)',
+                background: 'var(--colors-primary-bg-subtle, #FCEAE3)'
+            },
         },
         {
             logo:bolt,
             name: 'SUBSTATION',
             button: 'Go to substation',
             zone: 'Zone S',
+            zonestyle:{
+                color: 'var(--colors-primary-fg-on-subtle, #E75420)',
+                background: 'var(--colors-primary-bg-subtle, #FCEAE3)'
+            },
             region: 'Region XYZ',
             regionFlag: flag,
         },
     ]
 
+    const tooltip={
+        place:"left",
+        text:`Search for specific assets ( switches, transformers, lines, telemeters, counters and busbars). Click on the name of the asset from the dropdownlist and you’ll be redirected to the diagram view.`
+    }
+
 
     return (
         <div className={classes.container}>
-            <Topbar title={toptitle} />
+            <Topbar title={toptitle} tooltip={tooltip}/>
             <hr></hr>
             <QuickSelect />
             <ServiceRequest objectData={serviceRequest} />
--- src/shared/resultCard.js
+++ src/shared/resultCard.js
@@ -8,7 +8,7 @@ const ResultCard = (props) => {
         <div className={classes.main}>
             <div className={classes.upperpart}>
                 <div className={classes.section1}>
-                    <img src={props.logo} alt="bolt" /> 
+                    <img src={props.logo} alt="bolt" />
                     <p>{props.name}</p>
                 </div>
                 <div className={classes.section2}>
@@ -19,16 +19,14 @@ const ResultCard = (props) => {
                 </div>
             </div>
             <div className={classes.zone}>
-                <p style={{background:'var(--colors-primary-bg-subtle, #FCEAE3)', color:'var(--colors-primary-fg-on-subtle, #E75420)'}} >{props.zone}</p>
-                <span className={classes.region}>
-                    {props.region !== '' ? <>
+                <p style={props.zoneStyles} >{props.zone}</p>
+                {props.region !== undefined && <>
+                    <span className={classes.region}>
+                        <img src={props.regionFlag} alt="flag" />
+                        <p >{props.region}</p>
+                    </span>
+                </>}
 
-                            <img src={props.regionFlag} alt="flag" />
-                            <p >{props.region}</p>
-                        </>
-                        :<div></div>}
-                    
-                </span>
             </div>
         </div>
     )
